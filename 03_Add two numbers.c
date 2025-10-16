d78062dd515e84e187bd3c4e7f6256273addcfa7
029_HarshParmar <hp259369@gmail.com>
2025-10-17 03:34:04 +0530

    Add files via upload
    Add Two numbers which use only Two variables

---
+++ 03_Add two numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   // float value use becaue it's allow int. & float values
+    float a,b;
+    printf("Enter Value A: ");
+    scanf("%f",&a);
+    printf("Enter Value B: ");
+    scanf("%f",&b);
+    printf("A+B = %f",a+b);
+    return 0;
+
+}
