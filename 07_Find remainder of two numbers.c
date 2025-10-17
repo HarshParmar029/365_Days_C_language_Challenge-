913b6e9e1548820cc5d191635d2303f711eb4c6e
029_HarshParmar <hp259369@gmail.com>
2025-10-17 12:12:25 +0530

    Add files via upload
    Remainder means (Modulo [%]  Of 2 numbers)

---
+++ 07_Find remainder of two numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter Number A:");
+    scanf("%d",&a);
+    printf("Enter Number B : ");
+    scanf("%d",&b);
+    printf("Remainder(A%%B)=%d",a%b);
+}
