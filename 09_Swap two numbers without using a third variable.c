ae8dbb3098f5534cff5450457d3a9931b2af74c0
029_HarshParmar <hp259369@gmail.com>
2025-10-17 14:10:26 +0530

    Add files via upload
    Only 2  Variables use and get swap numbers

---
+++ 09_Swap two numbers without using a third variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter Number A : ");
+    scanf("%d",&a);
+    printf("Enter Number B : ");
+    scanf("%d",&b);
+    printf(" Number A=%d ,B=%d", b,a);
+    return 0;
+
+}
