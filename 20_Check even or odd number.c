a98b263a10a3910ba1a72f995fe3ef346c46e2a7
029_HarshParmar <hp259369@gmail.com>
2025-10-18 22:56:48 +0530

    Add files via upload

---
+++ 20_Check even or odd number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{   int a;
+    printf("enter number");
+    scanf("%d",&a);
+    if(a%2==0){
+    printf("Number is Even");
+    }
+    else{
+        printf("Number is odd");
+    }
+    return 0;
+}
