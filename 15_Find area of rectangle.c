c65fc97ff7020c49a92d2caadcd75372be21b8ff
029_HarshParmar <hp259369@gmail.com>
2025-10-18 17:13:00 +0530

    Add files via upload
    only Use 2 varaibles

---
+++ 15_Find area of rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float width,Length;
+    printf("Enter width :");
+    scanf("%f",&width);
+    printf("Enter Length :");
+    scanf("%f",&Length);
+    printf("Area Of rectangle %.2f",Length*width); //Area=Length*Width
+    return 0;
+}
