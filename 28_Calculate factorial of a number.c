5785c65d59a8e8811d9f08e2a73315e6d80aba59
029_HarshParmar <hp259369@gmail.com>
2025-10-19 11:16:40 +0530

    Add files via upload

---
+++ 28_Calculate factorial of a number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int fact = 1;
+    int i;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+     if(n < 0)
+     {
+        printf("Factorial of a negative number doesn't exist.\n");
+     }
+    else
+    {
+        for(i = 1; i <= n; i++)
+        {
+            fact = fact * i;
+        }
+        printf("Factorial of %d = %d\n", n, fact);
+    }
+    return 0;
+}
+
