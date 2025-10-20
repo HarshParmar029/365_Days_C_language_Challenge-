6f5dd37e1df4aa30cfb57507935ed6ffdd1f2e87
029_HarshParmar <hp259369@gmail.com>
2025-10-21 03:36:15 +0530

    Add files via upload

---
+++ 43_Print all even numbers between 1–100.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    printf("Even numbers between 1 and 100:\n");
+
+    for (i = 2; i <= 100; i += 2) {
+        printf("%d ", i);
+    }
+    return 0;
+}
+
