06a25cf07f0c0a5aa15b6e64dea018b9ca1a6361
029_HarshParmar <hp259369@gmail.com>
2025-10-23 00:14:52 +0530

    Add files via upload

---
+++ 78_Find mode of array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int n, i, j, maxCount = 0, mode;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    // Find mode (element with highest frequency)
+    for (i = 0; i < n; i++) {
+        int count = 0;
+        for (j = 0; j < n; j++) {
+            if (arr[j] == arr[i])
+                count++;
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            mode = arr[i];
+        }
+    }
+    printf("Mode = %d (appears %d times)\n", mode, maxCount);
+    return 0;
+}
+
